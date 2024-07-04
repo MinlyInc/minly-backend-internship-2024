216bfc2774e39754d7e51448674c25643e3392ab
AlyMBarakat <aly.m.barakat@gmail.com>
2024-07-04 18:42:20 +0300

    Added khalid's name

--- src/app.module.ts
+++ src/app.module.ts
@@ -15,6 +15,7 @@ import { HealthModule } from './health/health.module';
       database: process.env.DB_NAME,
       username: process.env.DB_USERNAME,
       password: process.env.DB_PASSWORD,
+      myNameIs: 'Khalid',
       entities: [],
       synchronize: true,
     }),
