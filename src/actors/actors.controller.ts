5729c34ba9f38887de3c8146136f55f3c8c74f61
KevinMarco <kevinmarco9000@gmail.com>
2024-07-25 14:38:45 +0300

    create a swagger for actor details

--- src/actors/actors.controller.ts
+++ src/actors/actors.controller.ts
@@ -1,12 +1,16 @@
 import { Controller, Get, Param } from '@nestjs/common';
 import { ActorsService } from './actors.service';
+import { ApiResponse, ApiTags } from '@nestjs/swagger';
+import { ActorDetailsResponse } from './swagger-model/actor-details';
 
+@ApiTags('actors')
 @Controller('actors')
 export class ActorsController {
     constructor(readonly actorsService: ActorsService){}
 
     
     @Get(':uuid')
+    @ApiResponse({ status: 200, description: 'Get actor details', type: ActorDetailsResponse })
     async getActorWithAwardsByUuid(@Param('uuid') uuid : string) {
         return this.actorsService.getActorWithAwardsByUuid(uuid) ;
     }
